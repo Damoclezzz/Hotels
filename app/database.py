ccbe4a207a41c0fb3a520cec65064a986613d8ab
Damoclezzz <vladislavkondor@gmail.com>
2023-07-27 22:13:45 +0300

    python-dotenv -> pydantic_settings in config

--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from config import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 
-DATABASE_URL = f'postgres+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+from config import settings
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
--- config.py
+++ config.py
@@ -1,11 +1,18 @@
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from dotenv import load_dotenv
 
-load_dotenv()
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
 
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
+    @property
+    def DATABASE_URL(self):
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()

e9886a03f8f0d9b9cd55f7fc8a4bcdf37cc0a727
Damoclezzz <vladislavkondor@gmail.com>
2023-07-27 22:22:09 +0300

    move DATABASE_URL to database.py

--- config.py
+++ config.py
@@ -8,10 +8,6 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_NAME: str
 
-    @property
-    def DATABASE_URL(self):
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
-
     model_config = SettingsConfigDict(env_file='.env')
 
 
--- migrations/env.py
+++ migrations/env.py
@@ -5,15 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from config import settings
-from app.database import Base
+from app.database import Base, DATABASE_URL
 from app.hotels.models import Hotel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option('sqlalchemy.url', f'{settings.DATABASE_URL}?async_fallback=True')
+config.set_main_option('sqlalchemy.url', f'{DATABASE_URL}?async_fallback=True')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.

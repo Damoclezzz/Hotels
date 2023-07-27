de221f302361ce8b462094d34592891eea703512
Damoclezzz <vladislavkondor@gmail.com>
2023-07-27 16:26:24 +0300

    Add Hotel model

---
+++ app/hotels/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import JSONB
+from app.database import Base
+
+
+class Hotel(Base):
+    __tablename__ = 'hotel'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    services = Column(JSONB)
+    rooms_quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)

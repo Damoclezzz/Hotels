e7fe99a156869227167781470475cd8ad4a506ea
Damoclezzz <vladislavkondor@gmail.com>
2023-07-28 17:46:15 +0300

    CODE-STYLE: rename dir hotels -> hotel

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.database import Base, DATABASE_URL
-from app.hotels.models import Hotel
+from app.hotel.models import Hotel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

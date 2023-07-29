1ee970b17af31c9c545630a79c9661682908225c
Damoclezzz <vladislavkondor@gmail.com>
2023-07-29 19:36:41 +0300

    ADD: filters to base query

--- src/database/service.py
+++ src/database/service.py
@@ -7,9 +7,10 @@ class BaseRepository:
     model = None
 
     @classmethod
-    async def find_all(cls):
+    async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model)
+            query = select(cls.model).filter_by(**filter_by)
+
             result = await session.execute(query)
 
             return result.mappings().all()

83da86ecb32daaab0f801266ddaefd36785ebd2a
Damoclezzz <vladislavkondor@gmail.com>
2023-07-29 13:15:14 +0300

    ADD: column "services" to Room model

--- src/room/models.py
+++ src/room/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy.dialects.postgresql import JSONB
 from src.database import Base
 
 
@@ -9,6 +10,7 @@ class Room(Base):
     hotel_id = Column(ForeignKey('hotel.id'))
     name = Column(String, nullable=False)
     description = Column(Text)
+    services = Column(JSONB)
     price = Column(Integer, nullable=False)
     quantity = Column(Integer, nullable=False, default=1)
     image_id = Column(Integer)

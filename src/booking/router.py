fbf501eef1564d5a7a86a1be736a76baade0cd08
Damoclezzz <vladislavkondor@gmail.com>
2023-07-29 19:37:20 +0300

    ADD: get_booking by id

--- src/booking/router.py
+++ src/booking/router.py
@@ -12,3 +12,8 @@ router = APIRouter(
 @router.get('')
 async def get_bookings():
     return await BookingService.find_all()
+
+
+@router.get('/{booking_id}')
+async def get_booking(booking_id: int):
+    return await BookingService.find_by_id(booking_id)

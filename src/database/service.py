81ee964cb9903009b569b50715026ed0f99b7e54
Damoclezzz <vladislavkondor@gmail.com>
2023-07-29 19:36:56 +0300

    ADD: new base queries

--- src/database/service.py
+++ src/database/service.py
@@ -7,6 +7,20 @@ class BaseRepository:
     model = None
 
     @classmethod
+    async def find_by_id(cls, model_id: int):
+        async with async_session_maker() as session:
+            return await session.get(cls.model, model_id)
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
+
+            result = await session.execute(query)
+
+            return result.mappings().one_or_none()
+
+    @classmethod
     async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)

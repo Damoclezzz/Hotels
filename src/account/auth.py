8f9f54cf0422f419136f35ee0cf37e707eddc334
Damoclezzz <vladislavkondor@gmail.com>
2023-08-11 11:36:07 +0300

    ADD: create_access_token func

--- src/account/auth.py
+++ src/account/auth.py
@@ -1,4 +1,8 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+
+from config import settings
 
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -10,3 +14,16 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=settings.TOKEN_MINUTES_LIFESPAN)
+    to_encode.update({'exp': expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode, settings.TOKEN_SECRET_KEY, settings.TOKEN_ALGORITHM
+    )
+
+    return encoded_jwt

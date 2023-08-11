120341a6cce193b8985ac812969640e1de801db1
Damoclezzz <vladislavkondor@gmail.com>
2023-08-11 11:38:37 +0300

    UPDATE: include router for account operations

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from src.booking.router import router as booking_router
+from src.account.router import router as account_router
 
 
 app = FastAPI()
 
 app.include_router(booking_router)
+app.include_router(account_router)
